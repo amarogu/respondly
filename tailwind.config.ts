4103e4199ca7cc7c6aec7e912ab2d1cb4c535631
Gustavo <gumarinho090306@gmail.com>
2024-01-02 20:44:42 -0300

    Update global styles and page layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,25 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'body-light': '#0A0908',
+        'accent': '#3066BE',
+        'papaya': '#FFEECF',
+        'body-dark': '#FFF8EBB3',
+        'body-dark-emphasized': '#FFF8EB',
+        'gray': '#1E293B',
+        'borders': '#E5E7EB',
+        'background': '#0B1121',
+        'background-light': '#F5F5F7',
+        "background-light-emphasized": "#FFFFFF",
+        "purple": "#4F00D6",
+        "blue": "#1D50DD",
+        "blue-two": "#0F5BEE",
+        "blue-three": "#0066FF",
+        "orange": "CA8F2E",
+        "orange-two": "#DF9318",
+        "orange-three": "#F59500"
+      }
     },
   },
   plugins: [],
