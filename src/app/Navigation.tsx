fab5a2704df884fe05d569eabdf26c92f05dc642
Gustavo <gumarinho090306@gmail.com>
2024-01-02 21:12:55 -0300

    Add dependencies and update layout and navigation

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import 'material-symbols';
 
 export default function Navigation() {
     return (
-        <nav>
-            <Image src="/Logomarca.svg" alt="Logomarca" width="100" height="100" />
+        <nav className="text-body-dark-emphasized">
+            <section className="flex gap-3">
+                <Image src="/Logomarca.svg" alt="Logomarca" width="100" height="100" />
+                <ul className="flex gap-2">
+                    <li>Início</li>
+                    <li>Planos</li>
+                    <li>Sobre</li>
+                    <span className="material-symbols-outlined">menu</span>
+                </ul>
+            </section>
         </nav>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-background`}>{children}</body>
+      <body className={`${inter.className} bg-background text-body-dark text-base`}>{children}</body>
     </html>
   )
 }
