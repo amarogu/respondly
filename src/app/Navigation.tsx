417d0b3f9de5ded9cc1e09ec3520f2702989036e
Gustavo <gumarinho090306@gmail.com>
2024-01-03 21:34:36 -0300

    Update Navigation and layout styles

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -11,12 +11,17 @@ export default function Navigation() {
 
     const toggle = () => {
         setOpen(!open); // toggle between true and false
+        if (!open) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'unset';
+        }
     };
 
     return (
-        <nav className="text-body-dark-emphasized flex items-center justify-between text-[18px] flex-wrap">
+        <nav className="text-body-dark-emphasized flex items-center justify-between flex-wrap">
             <section className="flex items-center gap-5">
-                <Image src="/Logomarca.svg" alt="Logomarca" width={140} height={80} />
+                <Image src="/Logomarca.svg" alt="Logomarca" width={100} height={80} className="sm:mt-[2px]" />
                 <ul className="sm:flex gap-2 hidden">
                     <li>Início</li>
                     <li>Planos</li>
@@ -24,10 +29,10 @@ export default function Navigation() {
                 </ul>
             </section>
             <button onClick={toggle}>
-                {open ? <CloseIcon className="sm:hidden" fontSize="large" /> : <MenuIcon className="sm:hidden" fontSize="large" />}
+                {open ? <CloseIcon className="sm:hidden" fontSize="medium" /> : <MenuIcon className="sm:hidden" fontSize="medium" />}
             </button>
             <Button className="hidden sm:block" text="Entrar" />
-            <div className={` bg-sky-800 sm:hidden w-screen left-0 top-0 transition-all rounded overflow-hidden absolute -z-10 ${open ? "opacity-1 h-screen" : "opacity-0 h-0"}`} >
+            <div className={`bg-background sm:hidden w-screen left-0 top-0 transition-all rounded overflow-hidden absolute -z-10 ${open ? "opacity-1 h-screen" : "opacity-0 h-0"}`} >
                 <div className="p-8">
                     <ul className="flex pt-16 flex-col gap-4">
                         <li>Início</li>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-background text-body-dark text-base`}>{children}</body>
+      <body className={`${inter.className} bg-background text-body-dark text-sm`}>{children}</body>
     </html>
   )
 }
