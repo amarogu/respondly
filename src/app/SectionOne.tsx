92dd539e98be8ef212e244d4d903bc7bc865e521
Gustavo <gumarinho090306@gmail.com>
2024-01-06 19:19:17 -0300

    Update SectionOne styling and background position

--- src/app/SectionOne.tsx
+++ src/app/SectionOne.tsx
@@ -5,15 +5,15 @@ import Phone2 from '../../public/phone2.png';
 
 export default function SectionOne() {
     return (
-        <section className="flex flex-col sm:flex-row gap-8 sm:justify-between">
-            <div className="flex flex-col gap-4 items-center text-center">
+        <section className="flex flex-col sm:flex-row gap-8 sm:justify-between sm:mt-48">
+            <div className="flex flex-col sm:items-start sm:text-left gap-4 items-center text-center">
                 <h2 className="text-4xl font-bold text-body-dark-emphasized">Conteúdo <span className="bg-line">palavra</span></h2>
                 <p className="max-w-72">
                 Eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at varius.
                 </p>
                 <Button text="Clique" />
             </div>
-            <div className="flex items-start gap-4">
+            <div className="flex items-start gap-4 lg:w-[55%] sm:gap-4 lg:gap-24">
                 <Image src={Phone1} alt="WhatsApp" />
                 <Image src={Phone2} className="mt-8" alt="Instagram" />
             </div>
