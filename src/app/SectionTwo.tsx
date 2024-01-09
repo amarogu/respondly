8aecaa5254716590e27af76826afc579b660dac2
Gustavo <gumarinho090306@gmail.com>
2024-01-09 18:02:35 -0300

    Update SectionTwo component

--- src/app/SectionTwo.tsx
+++ src/app/SectionTwo.tsx
@@ -1,12 +1,12 @@
 import Button from "./Button";
-import SocialDesktop from '../../public/socialdesktop.png';
+import SocialDesktop from '../../public/social@2x.png';
 import Image from "next/image";
 import Topic from "./Topic";
 
 export default function SectionTwo() {
     return(
-        <section className="flex flex-col sm:flex-row sm:justify-between">
-            <div className="flex flex-col gap-4 sm:order-1 sm:justify-center">
+        <section className="flex flex-col sm:flex-row sm:justify-between sm:gap-28">
+            <div className="flex flex-col gap-4 sm:order-1">
                 <h2 className="text-4xl font-bold text-body-dark-emphasized">Conteúdo Palavra</h2>
                 <p className="max-w-72">Eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at varius.</p>
                 <ul className="flex flex-col gap-2">
@@ -15,7 +15,7 @@ export default function SectionTwo() {
                     <Topic text="Telegram: description" />
                 </ul>
             </div>
-            <Image src={SocialDesktop} className="sm:w-[60%] sm:-translate-x-20" alt="Mídias sociais" />
+            <Image src={SocialDesktop} className="sm:w-[30%]" alt="Mídias sociais" />
         </section>
     )
 }
