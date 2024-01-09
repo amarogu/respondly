65b2e240721cb3be6c2ba26b901439ee53e60bf4
Gustavo <gumarinho090306@gmail.com>
2024-01-09 17:39:04 -0300

    Update SectionTwo component layout

--- src/app/SectionTwo.tsx
+++ src/app/SectionTwo.tsx
@@ -8,14 +8,14 @@ export default function SectionTwo() {
         <section className="flex flex-col sm:flex-row sm:justify-between">
             <div className="flex flex-col gap-4 sm:order-1 sm:justify-center">
                 <h2 className="text-4xl font-bold text-body-dark-emphasized">Conteúdo Palavra</h2>
-                <p>Eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at varius.</p>
+                <p className="max-w-72">Eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at varius.</p>
                 <ul className="flex flex-col gap-2">
                     <Topic text="Whatsapp: description" />
                     <Topic text="Instagram: description" />
                     <Topic text="Telegram: description" />
                 </ul>
             </div>
-            <Image src={SocialDesktop} className="sm:w-1/2" alt="Mídias sociais" />
+            <Image src={SocialDesktop} className="sm:w-[60%] sm:-translate-x-20" alt="Mídias sociais" />
         </section>
     )
 }
